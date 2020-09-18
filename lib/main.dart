5530710fd0815071cc77f66b8e38f6b37b834620
DOWGLAS <dowglasmaia@live.com>
2020-09-18 11:26:16 -0300

    impl regras para calculos do IMC

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String _infoText = "Informe seus dados!";
+
+  void _restFields() {
+    setState(() {
+      pesoController.text = "";
+      alturaController.text = "";
+
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calcularDados() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+
+      // altura deve ser convertido para metros
+      double altura = double.parse(alturaController.text) / 100;
+
+      /* O cálculo do IMC é feito dividindo o peso (em quilogramas) pela altura (em metros) ao quadrado*/
+      double imc = peso / (altura * altura);
+
+      print(imc);
+      /* Validações */
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levimente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,9 +65,12 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.green,
         //acções e icon
         actions: <Widget>[
+          /*BUTÃO REFRESH*/
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: () {
+              _restFields();
+            },
           )
         ],
       ),
@@ -48,6 +93,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.2),
+              controller: pesoController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -57,6 +103,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.2),
+              controller: alturaController,
             ),
 
             /* ### butão ###*/
@@ -64,8 +111,10 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
               child: Container(
                 height: 50.0,
-                child: RaisedButton(
-                  onPressed: () {},
+                child:
+                    /*BOTÃO CALCULAR*/
+                    RaisedButton(
+                  onPressed: _calcularDados,
                   child: Text(
                     "Calcular",
                     style: TextStyle(color: Colors.white, fontSize: 25.0),
@@ -75,7 +124,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              "Info",
+              _infoText,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
             ),
